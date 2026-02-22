cf79c37f5b18995ebef7b58c0176c39e3a77e026
Marc Odermatt <info@marcodermatt.ch>
2026-02-22 15:44:58 +0100

    footer

--- src/app/pages/legal/imprint/imprint.component.ts
+++ src/app/pages/legal/imprint/imprint.component.ts
@@ -12,6 +12,7 @@ export class ImprintComponent {
 
   labels: Record<string, Record<string, string>> = {
     title: { en: 'Legal Notice', de: 'Impressum' },
+    sectionTitle: { en: 'Imprint', de: 'Impressum' },
     contactTitle: { en: 'Exploring the Board', de: 'Exploring the Board' },
     acceptanceTitle: { en: 'Acceptance of terms', de: 'Annahme der Bedingungen' },
     scopeTitle: { en: 'Scope and ownership of the product', de: 'Umfang und Eigentum des Produkts' },
